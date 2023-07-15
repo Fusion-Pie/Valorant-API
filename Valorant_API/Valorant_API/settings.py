c8421a3b146abccc802b6bfcf80b9faac77d5aa2
Fusion-Pie <piyushhatewar@gmail.com>
2023-07-16 00:42:02 +0530

    Adding basic Pages and sorted fields od api

--- Valorant_API/Valorant_API/settings.py
+++ Valorant_API/Valorant_API/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Valorant_API.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Valorant_API/Valorant_app/views.py
+++ Valorant_API/Valorant_app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render, HttpResponse
 
+import requests
+
 # Create your views here.
 def home(request):
-    return HttpResponse("This is the content")
+    info = requests.get("https://valorant-api.com/v1/agents", params=None).json()
+
+    for data in info['data']:
+        if data['uuid'] == "e370fa57-4757-3604-3648-499e1f642d3f":
+            context = {
+                'uuid': data['uuid'],
+                'displayName': data['displayName'],
+                'desc': data['description'],
+                'displayIcon': data['displayIcon'], 
+                'bg': data['background']
+            }
+
+    return render(request, 'basePage.html', context=context)
+            
+    # e370fa57-4757-3604-3648-499e1f642d3f
