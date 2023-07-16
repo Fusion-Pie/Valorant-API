644959581cefae209374d4c389e5b4223d6262b1
Fusion-Pie <piyushhatewar@gmail.com>
2023-07-16 21:38:10 +0530

    Basic UI and some dynamicity in showing agents is done

--- Valorant_API/Valorant_app/views.py
+++ Valorant_API/Valorant_app/views.py
@@ -7,15 +7,17 @@ def home(request):
     info = requests.get("https://valorant-api.com/v1/agents", params=None).json()
 
     for data in info['data']:
-        if data['uuid'] == "e370fa57-4757-3604-3648-499e1f642d3f":
+        if data['displayName'] == "Brimstone":
             context = {
                 'uuid': data['uuid'],
                 'displayName': data['displayName'],
                 'desc': data['description'],
                 'displayIcon': data['displayIcon'], 
-                'bg': data['background']
+                'bg': data['background'],
+                'fullPortraitV2': data['fullPortraitV2'],
+                'role': data['role']['displayName']
             }
 
-    return render(request, 'basePage.html', context=context)
+    return render(request, 'index.html', context=context)
             
     # e370fa57-4757-3604-3648-499e1f642d3f
