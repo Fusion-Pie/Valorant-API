12890406202172154a282e92204c179a13380ccd
Fusion-Pie <piyushhatewar@gmail.com>
2023-07-16 22:47:21 +0530

    Added all agents name making them horizontal scroll and displying agent information on click

--- Valorant_API/Valorant_app/views.py
+++ Valorant_API/Valorant_app/views.py
@@ -2,22 +2,45 @@ from django.shortcuts import render, HttpResponse
 
 import requests
 
+
+# Hitting the API to get agents information
+info = requests.get("https://valorant-api.com/v1/agents", params=None).json()
+
+
+
+
 # Create your views here.
 def home(request):
-    info = requests.get("https://valorant-api.com/v1/agents", params=None).json()
+    if request.method == "POST":
+        agent_name = request.POST.get('btn_name')
+        print(agent_name.capitalize())
+        
+        for data in info['data']:
+            if data['displayName'] == agent_name.capitalize():
+                context = {
+                    'uuid': data['uuid'],
+                    'displayName': data['displayName'],
+                    'desc': data['description'],
+                    'displayIcon': data['displayIcon'], 
+                    'bg': data['background'],
+                    'fullPortraitV2': data['fullPortraitV2'],
+                    'role': data['role']['displayName']
+                }
 
-    for data in info['data']:
-        if data['displayName'] == "Brimstone":
-            context = {
-                'uuid': data['uuid'],
-                'displayName': data['displayName'],
-                'desc': data['description'],
-                'displayIcon': data['displayIcon'], 
-                'bg': data['background'],
-                'fullPortraitV2': data['fullPortraitV2'],
-                'role': data['role']['displayName']
-            }
+        return render(request, 'index.html', context=context)
 
-    return render(request, 'index.html', context=context)
-            
-    # e370fa57-4757-3604-3648-499e1f642d3f
+    else:
+        for data in info['data']:
+            if data['displayName'] == "Brimstone":
+                context = {
+                    'uuid': data['uuid'],
+                    'displayName': data['displayName'],
+                    'desc': data['description'],
+                    'displayIcon': data['displayIcon'], 
+                    'bg': data['background'],
+                    'fullPortraitV2': data['fullPortraitV2'],
+                    'role': data['role']['displayName']
+                }
+
+        return render(request, 'index.html', context=context)
+        
