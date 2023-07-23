4b0b6991ce49e502834573e5eb6832020971464b
Fusion-Pie <piyushhatewar@gmail.com>
2023-07-23 12:46:44 +0530

    Optimized:  1) html code to show agent names 2) created functions

--- Valorant_API/Valorant_app/views.py
+++ Valorant_API/Valorant_app/views.py
@@ -4,7 +4,7 @@ import requests
 
 
 # Hitting the API to get agents information
-info = requests.get("https://valorant-api.com/v1/agents", params=None).json()
+agent_endpoint = requests.get("https://valorant-api.com/v1/agents", params=None).json()
 
 
 # Create your views here.
@@ -12,33 +12,44 @@ def home(request):
     if request.method == "POST":
         agent_name = request.POST.get('btn_name')
         
-        for data in info['data']:                                  # This is done for sova              # This is done for kay/o
-            if (data['displayName'] == agent_name.capitalize() and data['isPlayableCharacter']) or data['displayName'] == agent_name:
-                context = {
-                    'uuid': data['uuid'],
-                    'displayName': data['displayName'],
-                    'desc': data['description'],
-                    'displayIcon': data['displayIcon'], 
-                    'bg': data['background'],
-                    'fullPortraitV2': data['fullPortraitV2'],
-                    'role': data['role']['displayName']
-                }
-
+        context = getAgentInfo(agent_name)
+        
+        context['agentList'] = getAgentsName()
 
         return render(request, 'index.html', context=context)
 
     else:
-        for data in info['data']:
-            if data['displayName'] == "Brimstone":
-                context = {
-                    'uuid': data['uuid'],
-                    'displayName': data['displayName'],
-                    'desc': data['description'],
-                    'displayIcon': data['displayIcon'], 
-                    'bg': data['background'],
-                    'fullPortraitV2': data['fullPortraitV2'],
-                    'role': data['role']['displayName']
-                }
+        # It is for the first time when the page gets loaded
+        context = getAgentInfo("Brimstone")
+        
+        context['agentList'] = getAgentsName()
 
         return render(request, 'index.html', context=context)
         
+
+# Function to get agent information
+def getAgentInfo(agentName):
+    for data in agent_endpoint['data']:
+        # isPlayableCharacter is used as the api has two sova's one is playable and another is not
+        if (data['displayName'] == agentName and data['isPlayableCharacter']):
+            context = {
+                'uuid': data['uuid'],
+                'displayName': data['displayName'],
+                'desc': data['description'],
+                'displayIcon': data['displayIcon'], 
+                'bg': data['background'],
+                'fullPortraitV2': data['fullPortraitV2'],
+                'role': data['role']['displayName']
+            }
+
+    return context
+
+
+# Function to get all agents name
+def getAgentsName():
+    agentList = []
+    for agent in agent_endpoint['data']:
+        if agent['isPlayableCharacter']:
+            agentList.append(agent['displayName'])
+
+    return agentList
